ee9c6fc1bcde471164c981efbcc187c189fb1e24
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-31 21:49:38 +0800

    Delete Python3 Tutorials directory

--- Python3 Tutorials/6_Modules/package/module3.py
+++
@@ -1,9 +0,0 @@
-
-def package_module3_function1():
-    print("package_module3_function1()")
-
-def package_module3_function2():
-    print("package_module3_function2()")
-
-
-__all__ = ['package_module3_function1']
